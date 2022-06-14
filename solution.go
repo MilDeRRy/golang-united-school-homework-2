24b45eaae3ceff049181d5098be02f2a35079844
MilDeRRy <bane009@yandex.ru>
2022-06-14 16:18:11 +0400

    room1

--- solution.go
+++ solution.go
@@ -1,5 +1,19 @@
 package square
 
+import (
+	"fmt"
+	"math"
+)
+
+type kolvo int
+
+/*func free() {
+	var sideLen float64
+	var sidesNum int
+	fmt.Scanln(&sideLen, &sidesNum)
+	fmt.Println(CalcSquare)
+}*/
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +22,23 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+func CalcSquare(sideLen float64, sidesNum kolvo) float64 {
+	var s float64
+	var hil float64
+	switch {
+	case sidesNum == 3:
+		hil = math.Sqrt(3)
+		s = (sideLen * sideLen * hil) / 4
+
+	case sidesNum == 4:
+		s = sideLen * sideLen
+
+	case sidesNum == 0:
+		s = math.Pi * sideLen * sideLen
+
+	default:
+		var v string = "error"
+		fmt.Print(v)
+	}
+	return s
 }
